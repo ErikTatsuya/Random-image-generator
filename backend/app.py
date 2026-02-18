814c265d48ee04b1f6a41e2bd5a0ddd724273722
ErikTatsuya <erik.tatsuya@gmail.com>
2026-02-18 01:26:58 -0300

    I think backend is finished finally. too tired to do front

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,24 @@
-from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI
 import sizeCalculator
 import imageGenerator
 import os
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",  # vite
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
